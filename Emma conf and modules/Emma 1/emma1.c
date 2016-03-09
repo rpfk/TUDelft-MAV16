2b34419eefcec8e5be63eeb70634420f0eb15808
GitHubber14 <w.plaetinck@student.tudelft.nl>
2016-03-09 09:32:34 +0100

    lol

--- Emma conf and modules/Emma 1/emma1.c
+++ Emma conf and modules/Emma 1/emma1.c
@@ -51,39 +51,39 @@ uint8_t emma69(uint8_t waypoint) {
 	float wp2_y = 2.;
 	float wp3_x = -2.;
 	float wp3_y = 2.;
-	float dist_threshold = 0.0001;
+	float dist_threshold = 0.1;
 	double wps[6] = {wp1_x, wp1_y, wp2_x, wp2_y, wp3_x, wp3_y};
-	int i = 0;
+	int i = 1;
 
-	struct EnuCoor_i *pos = stateGetPositionEnu_i(); // Get your current position 
+	struct EnuCoor_f *pos = stateGetPositionEnu_f(); // Get your current position 
 	
-	printf("Current pos \n");
-        printf("posX= %f \n", POS_FLOAT_OF_BFP(pos->x));
-        printf("posY= %f \n", POS_FLOAT_OF_BFP(pos->y));
+	printf("Current pos \t");
+        printf("posX= %f \t", pos->x); //POS_FLOAT_OF_BFP(pos->x)
+        printf("posY= %f \t", pos->y); //POS_FLOAT_OF_BFP(pos->y)
         printf("\n");
        
 	float wpX = waypoint_get_x(waypoint);
 	float wpY = waypoint_get_y(waypoint);
         
-	printf("Current wp \n");
-        printf("wpX: %f \n", wpX);
-        printf("wpY: %f \n", wpY);
+	printf("Current wp \t");
+        printf("wpX: %f \t", wpX);
+        printf("wpY: %f \t", wpY);
         printf("\n");
         
-	float dist_curr = (wpX -  POS_FLOAT_OF_BFP(pos->x))*(wpX -  POS_FLOAT_OF_BFP(pos->x)) + (wpY -  POS_FLOAT_OF_BFP(pos->y))*(wpY -  POS_FLOAT_OF_BFP(pos->y)); // POS_BFP_OF_REAL
+	float dist_curr = (wpX -  pos->x)*(wpX -  pos->x) + (wpY -  pos->y)*(wpY -  pos->y); // POS_BFP_OF_REAL POS_FLOAT_OF_BFP(pos->x)
 
-	printf("Dist to current wp \n");	
-        printf("dist_curr: %f \n", dist_curr);        
+	printf("Dist to current wp \t");	
+        printf("dist_curr: %f \t", dist_curr);        
         printf("\n");
 
-        float dist1 = (wpX - wp1_x)*(wpX - wp1_x) + (wpY - wp1_y)*(wpY - wp1_y); // Dist between current wp and navigation wps
-	float dist2 = (wpX - wp2_x)*(wpX - wp2_x) + (wpY - wp2_y)*(wpY - wp2_y);
-	float dist3 = (wpX - wp3_x)*(wpX - wp3_x) + (wpY - wp3_y)*(wpY - wp3_y);
+        //float dist1 = (wpX - wp1_x)*(wpX - wp1_x) + (wpY - wp1_y)*(wpY - wp1_y); // Dist between current wp and navigation wps
+	//float dist2 = (wpX - wp2_x)*(wpX - wp2_x) + (wpY - wp2_y)*(wpY - wp2_y);
+	//float dist3 = (wpX - wp3_x)*(wpX - wp3_x) + (wpY - wp3_y)*(wpY - wp3_y);
 	
 	
-	if (dist1 < dist2 && dist1 < dist3){i=1;} 
-	else if (dist2 < dist1 && dist2 < dist3){i=2;}
-	else {i=3;}
+	//if (dist1 < dist2 && dist1 < dist3){i=1;} 
+	//else if (dist2 < dist1 && dist2 < dist3){i=2;}
+	//else {i=3;}
 
 	if (dist_curr < dist_threshold*dist_threshold){
 		i = i + 1;
@@ -91,16 +91,16 @@ uint8_t emma69(uint8_t waypoint) {
 	}
 
 	// Set the waypoint to the calculated position
-        printf("Set waypoint to \n");
-	printf("i: %d \n", i);
-	printf("wpsX: %f \n",wps[(i-1)*2]);
-	printf("wpsY: %f \n",wps[(i-1)*2+1]);
+        printf("Set waypoint to \t");
+	printf("i: %d \t", i);
+	printf("wpsX: %f \t",wps[(i-1)*2]);
+	printf("wpsY: %f \t",wps[(i-1)*2+1]);
         printf("\n");
 	waypoint_set_xy_i(waypoint, wps[(i-1)*2], wps[(i-1)*2+1]);
         
 	if (count < 2) {count = 2;}
 	else {count = 1;}
-        printf("Count: %d \n", count);
+        printf("Count: %d \t", count);
         printf("\n");
 
 	return FALSE;
